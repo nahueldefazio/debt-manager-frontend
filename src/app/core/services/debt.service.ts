29a8ff750edc3bdddbfad2dd52d30a16ad2733f2
Nahuel De Fazio <nahuel1116@gmail.com>
2026-04-21 16:54:52 -0300

    fix: correct environment import path

--- src/app/core/services/debt.service.ts
+++ src/app/core/services/debt.service.ts
@@ -2,7 +2,7 @@ import { Injectable, inject } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
 import { Debt } from '../../models/debt.model';
-import { environment } from '../../../../environments/environment';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
